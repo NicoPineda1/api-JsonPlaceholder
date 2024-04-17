e4131305ec65a77552197758293a9dc4215bef11
NicoPineda1 <153849383+NicoPineda1@users.noreply.github.com>
2024-04-17 11:22:40 -0500

    api jsonplaceholder

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Photos from './componentes/Photos';
+
+    const App = () => {
+      return(
+       <>
+       <h1>Photos</h1>
+       <Photos/>
+       </>
+       )
+    
+    };
+    
+
+export default App
